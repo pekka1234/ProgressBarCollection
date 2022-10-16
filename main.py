09eed80fc68fc1668539f37d9e2eefc14ce468e5
pekka1234 <62663286+pekka1234@users.noreply.github.com>
2022-10-16 17:57:07 +0300

    version 0.2

--- main.py
+++ main.py
@@ -3,41 +3,43 @@ from random import randint
 import os
 import sys
 import time
-import cursor
 from tqdm import *
 from alive_progress import *
 from halo import Halo
 from yaspin import yaspin
 from yaspin.spinners import Spinners
 
-cursor.hide()
+def tqdm_func():
+    barCunt = int(input("Enter amount of progress bars:\n"))
+    if barCunt > 1:
+        speeds = input("Enter all progress finishing time (in seconds and seperated by a ',', for exmp. 12,44,13):\n")
+    else:
+        speeds = input("Enter finishing time (in seconds):\n")
+    print(barCunt,speeds)        
 
-def tqdm():
-    print("nogay")
-
-def halo():
+def halo_func():
     print("gay")
 
-def yaspin():
+def yaspin_func():
     print("2gays")
 
-def alive():
+def alive_func():
     print("3gays")
 
-def bash():
+def bash_func():
     print("4gays")
 
-def progress():
+def progress_func():
     print("5gays")    
 
-def showtime():
+def showtime_func():
     print("6gays")    
 
 print("Progress bars:\n0:tqdm\n1:halo\n2:yaspin\n3:alive_progress\n4:bash\n5:progress\n6:showtime(alive_progress)")
 bars = ["tqdm", "halo", "yaspin", "alive", "bash", "progress", "showtime"]
 bar = input("Enter progress bar (by number):\n")
-
-eval(f"{bars[int(bar)]}()")
+numToFuncDic = {"0":"tqdm_func()", "1":"halo_func()", "2":"yaspin_func()", "3":"alive_func()", "4":"bash_func()", "5":"progress_func()", "6":"showtime_func()"}
+eval(numToFuncDic[bar])
 
 
 

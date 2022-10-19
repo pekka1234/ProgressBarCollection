4feaed799563966062d47c78d885d6233a28d173
pekka1234 <62663286+pekka1234@users.noreply.github.com>
2022-10-19 14:52:00 +0300

    version 0.6

--- main.py
+++ main.py
@@ -11,29 +11,11 @@ import sys
 import time
 
 
-barCunt, speeds, smooth, blocks = None, None, None, None
-
-
-def asking():
-    global barCunt, speeds, smooth, blocks
-    print("Progress bars:\n0:tqdm\n1:halo\n2:yaspin\n3:alive_progress\n4:bash\n5:progress\n6:showtime(alive_progress)")
-    bars = ["tqdm", "halo", "yaspin", "alive", "bash", "progress", "showtime"]
-    bar = input("Enter progress bar (by number):\n")
-    barCunt = int(input("Enter amount of progress bars:\n"))
-    if barCunt > 1:
-        speeds = input("Enter all progress finishing time (in seconds and seperated by a ',', for exmp. 12,44,13):\n")
-    else:
-        speeds = input("Enter finishing time (in seconds):\n")
-    speeds = [int(x) for x in speeds.split(",")]
-    smooth = input("Do you want to the bars to run smootly or more cryomplhy? [y or n]:\n")
-    smooth = "y" == smooth
-    blocks = int(input("In how many chonks do you want the progress bars be?:\n"))
-    numToFuncDic = {"0":"tqdm_func()", "1":"halo_func()", "2":"yaspin_func()", "3":"alive_func()", "4":"bash_func()", "5":"progress_func()", "6":"showtime_func()"}
-    eval(numToFuncDic[bar])
-
 
 def tqdm_func():
-    global barCunt, speeds, smooth, blocks
+    speeds = [int(x) for x in input("Enter all progress finishing time (in seconds and seperated by a ',', for exmp. 12,44,13):\n").split(",")]
+    smooth = input("Do you want to the bars to run smootly or more cryomplhy? [y or n]:\n") == "y"
+    blocks = int(input("In how many chonks do you want the progress bars be?:\n"))
     if smooth:
         for x in range(len(speeds)):
             for y in tqdm(range(blocks)):
@@ -48,13 +30,20 @@ def tqdm_func():
                     if total_time - t > 0:
                         total_time -= t
                         time.sleep(t)
-
                 else:
                     time.sleep(total_time)
 
 
 def halo_func():
-    print("gay")
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    messages = input("Enter messager for each bar (seperated by a colin for exmp. Loading..., Finished!):\n").split(",")
+    spinner = Halo(text=messages[0], spinner='dots')
+    for x in messages:
+        spinner.start(x)
+        time.sleep(1)
+        spinner.succeed()
+
+
 
 def yaspin_func():
     print("2gays")
@@ -72,5 +61,8 @@ def showtime_func():
     print("6gays")
 
 
-if __name__ == "__main__":
-    asking()
+print("Progress bars:\n0:tqdm\n1:halo\n2:yaspin\n3:alive_progress\n4:bash\n5:progress\n6:showtime(alive_progress)")
+bars = ["tqdm", "halo", "yaspin", "alive", "bash", "progress", "showtime"]
+bar = input("Enter progress bar (by number):\n")
+numToFuncDic = {"0":"tqdm_func()", "1":"halo_func()", "2":"yaspin_func()", "3":"alive_func()", "4":"bash_func()", "5":"progress_func()", "6":"showtime_func()"}
+eval(numToFuncDic[bar])
